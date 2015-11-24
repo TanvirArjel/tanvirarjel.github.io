a80802b40aac3a538570ee93158ad39196e5341e
Tanvir <tanvirdu7@gmail.com>
2015-11-24 23:46:06 +0600

    ikon

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+
+   
+    $(window).load(function(){
+
+      // FlexSlider Section   
+
+      $('.flexslider').flexslider({
+        animation: "slide",
+        slideshowSpeed: 2500,
+        animationSpeed: 1000,
+
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    
+
+
+// textillate
+
+
+
+    $('.section-heading').textillate({
+        in: { effect: 'rollIn', delay:200},
+        out: { effect: 'rollOut', delay:200,
+        reverse: true,
+
+    },
+        loop: true
+    
+    });
+
+
+
+    $('.home-logo').textillate({
+        in: { effect: 'fadeInLeftBig', 
+        delay:150,
+        autoStart: true,
+},
+        out: { effect: 'fadeOutRightBig', 
+        delay:150,
+    reverse: true,
+},
+        loop: true
+    
+    });
+
+});
+
+
+// animsition/Home Page rotation
+
+  // $(document).ready(function() {
+  //   $('.animsition').animsition({
+  //       inClass: 'rotate-in-lg',
+  //       inDuration: 1000,
+
+  //   });
+  // });
