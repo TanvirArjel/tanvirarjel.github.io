79dc1120ac392c7dba048fd984e9080a63c5f75a
Tanvir Ahmad Arjel <Tanvir Ahmad Arjel>
2016-04-02 18:48:13 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+	$('.slider').fractionSlider({
+		'fullWidth': 			true,
+		'controls': 			true, 
+		'pager': 				true,
+		'responsive': 			true,
+		'dimensions': 			"1900,800",
+	    'increase': 			false,
+		'pauseOnHover': 		false
+	});
+
+});
