57097cadb44c76cc5b36a0889c4a9fd4377c63d8
Tanvir Ahmad Arjel <Tanvir Ahmad Arjel>
2016-04-06 00:58:15 +0600

    circle

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
-$(function() {
+// $(function() {
     
-    var $meters = $(".meter > .item, .item2");
-    var $section = $('#third');
-    var $queue = $({});
+//     var $meters = $(".meter > .item, .item2");
+//     var $section = $('#third');
+//     var $queue = $({});
     
-    function loadDaBars() {
-                $(".meter > .item, .item2 ").each(function() {
-                    $(this)
-                        .data("origWidth", $(this).width())
-                        .width(0)
-                        .animate({
-                            width: $(this).data("origWidth")
-                        }, 1200);
-                });
-    }
+//     function loadDaBars() {
+//                 $(".meter > .item, .item2 ").each(function() {
+//                     $(this)
+//                         .data("origWidth", $(this).width())
+//                         .width(0)
+//                         .animate({
+//                             width: $(this).data("origWidth")
+//                         }, 1200);
+//                 });
+//     }
     
-    $(document).bind('scroll', function(ev) {
-        var scrollOffset = $(document).scrollTop();
-        var containerOffset = $section.offset().top - window.innerHeight;
-        if (scrollOffset > containerOffset) {
-            loadDaBars();
-            // unbind event not to load scrolsl again
-            $(document).unbind('scroll');
-        }
-    });
+//     $(document).bind('scroll', function(ev) {
+//         var scrollOffset = $(document).scrollTop();
+//         var containerOffset = $section.offset().top - window.innerHeight;
+//         if (scrollOffset > containerOffset) {
+//             loadDaBars();
+//             // unbind event not to load scrolsl again
+//             $(document).unbind('scroll');
+//         }
+//     });
     
-});
+// });
+
+
+   
