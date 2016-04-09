bee650703ce9e3b71f9762d4622e79df315ab7fd
Tanvir Ahmad Arjel <Tanvir Ahmad Arjel>
2016-04-09 22:39:45 +0600

    css

--- js/circle-progress.js
+++ js/circle-progress.js
@@ -64,7 +64,7 @@ License: MIT
          * Animation config (see jQuery animations: http://api.jquery.com/animate/)
          */
         animation: {
-            duration: 3000,
+            duration: 1200,
             easing: 'circleProgressEasing'
         },
 
